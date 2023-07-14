18f292a43724af5519f1ce2a5417ea02bd44dc36
Igoryani4 <110982221+Igoryani4@users.noreply.github.com>
2023-07-14 10:42:48 +0300

    finished second task HW6

--- HW6_Modules/HW/chess.py
+++ HW6_Modules/HW/chess.py
@@ -1,18 +1,17 @@
 
 
-def chess(txt):
-    n = txt.split()
+def chess(position: list[list[int]]) -> bool:
+    n = len(position)
     x = []
     y = []
-    
-    for i in range(0, len (n) , 2):
-        new_x , new_y = int(n[i]) , int (n[i + 1])
+    for i in range(n):
+        new_x , new_y = position[i][0] , position[i][1]
         x.append(new_x)
         y.append(new_y)
 
     correct = True
-    for i in range(len (x)):
-        for j in range(i + 1, len (x)):
+    for i in range(n):
+        for j in range(i + 1, n):
             if x[i] == x[j] or y[i] == y[j] or abs(x[i] - x[j]) == abs(y[i] - y[j]):
                 correct = False
 

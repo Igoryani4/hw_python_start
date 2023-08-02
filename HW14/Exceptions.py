969b9254a59602d58c4850fe913fbd47b839ef9b
Igoryani4 <110982221+Igoryani4@users.noreply.github.com>
2023-08-02 16:55:55 +0300

    finished HW14

---
+++ HW14/Exceptions.py
@@ -0,0 +1,30 @@
+'''
+Задание №3
+Создайте класс с базовым исключением и дочерние классыисключения:
+○ ошибка уровня,
+○ ошибка доступа.
+'''
+
+class BasicExeption(Exception):
+    pass
+
+class LevelError(BasicExeption):
+    def __init__(self, value, value_min):
+        self.value = value
+        self.value_min = value_min
+
+
+
+    def __str__(self):
+        return f"Ошибка уровня - {self.value} > минимального уровня {self.value_min}"
+
+
+
+class AccesErorr(BasicExeption):
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return f"Ошибка доступа - {self.value}"
+
+
+

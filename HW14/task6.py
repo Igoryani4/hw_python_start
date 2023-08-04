435c07d0724cd7fa6227296d43cd948d356a0152
Igoryani4 <110982221+Igoryani4@users.noreply.github.com>
2023-08-04 13:42:09 +0300

    answers

--- HW14/task6.py
+++ HW14/task6.py
@@ -23,14 +23,14 @@ def test_access_fail_1(set_up):                               # тест на п
     with pytest.raises(AccesErorr, match='Access denied'):
         set_up.login('Greg', '1')                      # передаются невалидные данные
 
-
+@pytest.fixture()
 def test_access(set_up):                                    # тест на проверку валидных данных, то что должна вернуться
     assert set_up.login('bob', '1') == '1'             # вводятся валидные двнные и проверяем на совпадение с результатом
 
-
+@pytest.fixture()
 def test_access_fail_2(set_up):                                    # тест на проверку того, что  будет выброс исключения AccesErorr при вводе невалидных данных, AccesErorr  прописан в Exceptions.py
     with pytest.raises(LevelError):
-        set_up.login('bob', '1')
+        set_up.login('bob', '1')  
         set_up.create_user('New_user', '1', '3')
 
 
